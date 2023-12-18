a975ed34f6ff19f11b7f83bb1cd9092801a7ebec
Amerb908 <ab1571514@gmail.com>
2023-12-18 13:39:03 -0800

    Add project files.

---
+++ NETWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NETWebApp.Models;
+
+namespace NETWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<NETWebApp.Models.Joke>? Joke { get; set; }
+    }
+}
